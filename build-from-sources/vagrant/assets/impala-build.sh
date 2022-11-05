91332c6b2bf4c871c0fed5cb3a25fdc6d1b70cee
Javi Roman <jroman.espinar@gmail.com>
2022-11-05 09:50:57 +0100

    Fix #5 removed builder

--- build-from-sources/vagrant/assets/impala-build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-git clone https://github.com/apache/impala.git
-pushd /home/vagrant/build/impala/
-export IMPALA_HOME=`pwd`
-./bin/bootstrap_system.sh
-
-source ./bin/impala-config.sh
-./buildall.sh -noclean -notests
